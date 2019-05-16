a755dba5855ccedc81095c594303a2652a7df782
Paul Rouget <me@paulrouget.com>
2019-05-16 13:07:48 +0200

    build with rust-webvr

--- ServoApp/MainPage.xaml.cpp
+++ ServoApp/MainPage.xaml.cpp
@@ -1,16 +1,55 @@
 #include "pch.h"
 #include "MainPage.xaml.h"
 
+using namespace Windows::UI::Xaml::Controls;
 using namespace ServoApp;
+
+void Log(const wchar_t* msg) {
+  wchar_t msg2[256];
+  swprintf(msg2, 255, L"%s\r\n", msg);
+  OutputDebugString(msg2);
+}
+
+void LogWithError(const wchar_t* msg) {
+  wchar_t buf[256];
+  FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+      NULL, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      buf, (sizeof(buf) / sizeof(wchar_t)), NULL);
+  Log(msg);
+  Log(buf);
+}
+
+HMODULE LoadDLL(const wchar_t* name) {
+  HMODULE m = LoadPackagedLibrary(name, 0);
+  if (m == NULL) {
+    wchar_t error[256];
+    swprintf(error, 255, L"FAILED: %s", name);
+    LogWithError(error);
+  } else {
+	  wchar_t msg[256];
+	  swprintf(msg, 255, L"SUCCESS: %s", name);
+	  Log(msg);
+  }
+  return m;
+}
 
 MainPage::MainPage()
 {
-	InitializeComponent();
-	
-	HMODULE ret = LoadPackagedLibrary(L"hlservo.dll", 0);
-	FARPROC r = GetProcAddress(ret, "foobar");
-	INT_PTR ptr = r();
+  InitializeComponent();
 
-	int number = ptr;
+  HMODULE ret = LoadDLL(L"hlservo.dll");
 
+  FARPROC r = GetProcAddress(ret, "foobar");
+  if (r == NULL) {
+    LogWithError(L"GetProcAddress failed");
+    return;
+  }
+
+  int v = (int)r();
+
+  // std::string s_str = std::string(v);
+  // std::wstring wid_str = std::wstring(s_str.begin(), s_str.end());
+  // const wchar_t* w_char = wid_str.c_str();
+  //  Log(w_char);
 }
+
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,10 @@
-use std::cell::Cell;
+use rust_webvr as webvr;
 
 #[no_mangle]
 pub extern fn foobar() -> i32 {
-    let o = Cell::new(2);
-    o.get()
+    let mut vr = webvr::VRServiceManager::new();
+    vr.register_defaults();
+    vr.register_mock();
+    vr.initialize_services();
+    0
 }
